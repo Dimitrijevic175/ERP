9d74f6dfd9ee51a36852845973b3f6ae25f5cf92
Maksim Dimitrijevic <maksimdimitrijevic@gmail.com>
2025-12-18 20:48:34 +0100

    Delete product-service directory

--- product-service/src/main/java/com/dimitrijevic175/product_service/exceptions/CategoryDeletionNotAllowedException.java
+++
@@ -1,7 +0,0 @@
-package com.dimitrijevic175.product_service.exceptions;
-
-public class CategoryDeletionNotAllowedException extends RuntimeException {
-    public CategoryDeletionNotAllowedException(String reason) {
-        super("Category cannot be deleted: " + reason);
-    }
-}
