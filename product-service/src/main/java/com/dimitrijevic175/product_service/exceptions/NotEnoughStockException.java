dfb73734c6a4bf8534b29f3e6b941792fd5f1e79
Maksim Dimitrijevic <maksimdimitrijevic@gmail.com>
2025-12-18 20:47:47 +0100

    Delete product-service directory

--- product-service/src/main/java/com/dimitrijevic175/product_service/exceptions/NotEnoughStockException.java
+++
@@ -1,7 +0,0 @@
-package com.dimitrijevic175.product_service.exceptions;
-
-public class NotEnoughStockException extends RuntimeException{
-    public NotEnoughStockException() {
-        super("Not enough stock to decrease.");
-    }
-}
