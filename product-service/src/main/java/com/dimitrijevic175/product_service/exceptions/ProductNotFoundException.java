54a368e726087ede567016774fb431b2bd16d58d
Maksim Dimitrijevic <maksimdimitrijevic@gmail.com>
2025-12-23 12:56:32 +0100

    Delete product-service directory

--- product-service/src/main/java/com/dimitrijevic175/product_service/exceptions/ProductNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.dimitrijevic175.product_service.exceptions;
-
-public class ProductNotFoundException extends RuntimeException{
-    public ProductNotFoundException(Long id) {
-        super("Product not found with id: " + id);
-    }
-}
--- product-service/src/main/java/com/dimitrijevic175/product_service/exceptions/SkuNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.dimitrijevic175.product_service.exceptions;
-
-public class SkuNotFoundException extends RuntimeException {
-    public SkuNotFoundException(String id) {
-        super("SKU not found with id: " + id);
-    }
-
-}
