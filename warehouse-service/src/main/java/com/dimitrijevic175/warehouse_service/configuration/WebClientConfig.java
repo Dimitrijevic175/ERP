4cdf024a277cfcbef2f855a674270a7c152388e8
Maksim Dimitrijevic <mdimitrijevic7622rn@raf.rs>
2026-02-13 18:08:50 +0100

    cahnges made

--- warehouse-service/src/main/java/com/dimitrijevic175/warehouse_service/configuration/WebClientConfig.java
+++ warehouse-service/src/main/java/com/dimitrijevic175/warehouse_service/configuration/WebClientConfig.java
@@ -1,22 +1,29 @@
 package com.dimitrijevic175.warehouse_service.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+    @Value("${product.service.url}")
+    String productUrl;
+    @Value("${procurement.service.url}")
+    String procurementUrl;
+
     @Bean
     public WebClient productWebClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8081")
+                .baseUrl(productUrl)
                 .build();
     }
 
     @Bean
     public WebClient procurementWebClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8082")
+                .baseUrl(procurementUrl)
                 .build();
     }
 
